6a1ede491a5d20f4b754012c26c2f7793c9fa394
nguyenvietthai <nguyenvietthai1351997@gmail.com>
2019-03-18 23:54:36 +0700

    create hospital manager index

--- resources/views/admin/hospitals/index.blade.php
+++ resources/views/admin/hospitals/index.blade.php
@@ -10,12 +10,70 @@
 @endsection
 @section('pagename')
     <div class="container-fluid">
-        <div class="row">
+        <div class="row" style="background-color: white">
             <div class="col-lg-12">
                 <h1 class="page-header">Hospitals manager</h1>
-                <a href="/admin/hospitals/create">Create hospitals page</a>
-                <a href="/admin/hospitals/15/edit">Edit hospitals page</a>
 
+                <div class="body-box">
+                    
+                <!--Create , Edit -->
+                <div style="margin-bottom: 10px">
+                    <a href="#" type="button" class="btn btn-default">
+                    <span class="glyphicon glyphicon-plus-sign"></span>
+                     Create hospitals page
+                    </a>
+                    <a href="#" type="button" class="btn btn-info">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                     Edit hospitals page
+                    </a>   
+                </div>
+
+                <!--Table content-->
+                <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Customer</th>
+                            <th>Location</th>
+                            <th>Order date</th>
+                            <th>Status</th>
+                            <th>Net Amount</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Trident</td>
+                            <td>Internet
+                            Explorer 4.0
+                            </td>
+                            <td>Win 95+</td>
+                            <td> 4</td>
+                            <td>X</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Trident</td>
+                            <td>Internet
+                            Explorer 5.0
+                            </td>
+                            <td>Win 95+</td>
+                            <td>5</td>
+                            <td>C</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Rendering engine</th>
+                            <th>Browser</th>
+                            <th>Platform(s)</th>
+                            <th>Engine version</th>
+                            <th>CSS grade</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
             </div>
         </div>
     </div>
