c9cd4f29abd6ab946174f108c25989003bb31479
hhn21 <hoha.nguyen@gmail.com>
2019-05-04 18:46:13 +0700

    group 2 start adding into /doctor

--- resources/views/doctor/index.blade.php
+++ resources/views/doctor/index.blade.php
@@ -42,6 +42,10 @@
                 <p>Khoa: Chấn thương chỉnh hình</p>
                 <p>sdt: 0123456789</p>
                 <button type="button" class="btn btn-primary" style="margin-: 10px" data-toggle="modal" data-target="#infoModal">Cập nhật thông tin</button>
+                <!-- Nhóm 2  thêm button-->
+                <br><br>
+                <button type="button" class="btn btn-primary" style="margin-bottom: 10px" data-toggle="modal" data-target="#urgentModal">Thông báo khám khẩn cấp</button>
+                <!-- end Nhóm 2  thêm button-->
             </center>
 
         </div>
@@ -192,6 +196,53 @@
 
     </div>
 </div>
+<!-- Nhóm 2 thêm cửa sổ danh sách khám khẩn cấp -->
+<div id="urgentModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Thông tin cá nhân</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="{{asset('img/avatar.jpg')}}" class="img-responsive" alt="Cinque Terre">
+                    </div>
+                    <div class="col-sm-8">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="name">Họ tên</label>
+                                <input type="text" class="form-control" value="Lê Hoàng Vũ" disabled>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" value="vu.hl@gmail.com" disabled>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Số điện thoại:</label>
+                                <input type="number" class="form-control" value="0123456789">
+                            </div>
+                            <div class="form-group">
+                                <label for="address">Địa chỉ:</label>
+                                <input type="text" class="form-control" value="Thanh Xuân-Hà Nội">
+                            </div>
+                            <button type="button" id="update-password" class="btn btn-success">Thay đổi mật khẩu</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" id="update-info" class="btn btn-primary">Cập nhật</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- end Nhóm 2 thêm cửa sổ danh sách khám khẩn cấp -->
 <div id="passModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
 
