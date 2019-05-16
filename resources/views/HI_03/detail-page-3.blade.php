114e46b92aded1873b752f2f33c821ab75b9855c
Trịnh Hữu Vũ <vutrinhhuu@gmail.com>
2019-05-16 21:53:26 +0700

    complete appointment progress

--- resources/views/HI_03/detail-page-3.blade.php
+++ resources/views/HI_03/detail-page-3.blade.php
@@ -3,11 +3,29 @@
 @section('SPECIFIC CSS')
     <link href="{{ asset('HI_03/css/date_picker.css') }}" rel="stylesheet">
     <link href="{{ asset('HI_02/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('HI_03/css/progress_bar.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
     <main class="detail-page">
-        <div id="breadcrumb">
+        <div class="progress_bar">
+            <div class="bar__container">
+                <ul class="bar" id="bar">
+                    @if($appointment_type == "date")
+                        <li class="active">Chọn lịch khám</li>
+                        <li class="active">Chọn bệnh viện</li>
+                    @endif
+                    <li class="active">Chọn bác sĩ</li>
+                    @if($appointment_type == "date")
+                        <li class="active">Chọn hình thức khám</li>
+                    @elseif($appointment_type == "doctor")
+                        <li class="active">Chọn lịch khám</li>
+                    @endif
+                    <li>Thanh toán</li>
+                </ul>
+            </div>
+        </div>
+        {{-- <div id="breadcrumb">
             <div class="container">
                 <ul>
                     <li><a href="{{ url('/') }}">Trang chủ</a></li>
@@ -15,7 +33,7 @@
                     <li>Thông tin bác sĩ</li>
                 </ul>
             </div>
-        </div>
+        </div> --}}
         <!-- /breadcrumb -->
 
         <div class="container margin_60">
@@ -190,7 +208,7 @@
                                         </li>
                                     </ul>
                                     <hr>
-                                    <div class="text-center"><a href="{{ url('booking-page') }}" class="btn_1 medium">Tiếp tục</a>
+                                    <div class="text-center"><a href="{{ route('booking', ['appointment_type' => $appointment_type]) }}" class="btn_1 medium">Tiếp tục</a>
                                     </div>
                                 </div>
                             </div>
--- resources/views/HI_03/grid-list.blade.php
+++ resources/views/HI_03/grid-list.blade.php
@@ -2,12 +2,22 @@
 
 @section('SPECIFIC CSS')
 	<link href="{{ asset('HI_03/css/date_picker.css') }}" rel="stylesheet">
+	<link href="{{ asset('HI_03/css/progress_bar.css') }}" rel="stylesheet">
 @endsection
 
 
 @section('content')
 	<main>
-		<div id="breadcrumb">
+		<div class="progress_bar">
+			<div class="bar__container">
+				<ul class="bar" id="bar">
+					<li class="active">Chọn bác sĩ</li>
+					<li>Chọn lịch khám</li>
+					<li>Thanh toán</li>
+				</ul>
+			</div>
+		</div>
+		{{-- <div id="breadcrumb">
 			<div class="container">
 				<ul>
 					<li><a href="{{ url('/') }}">Trang chủ</a></li>
@@ -27,7 +37,7 @@
 				<!-- /row -->
 			</div>
 			<!-- /container -->
-		</div>
+		</div> --}}
 		<!-- /results -->
 		
 		<div class="filters_listing">
@@ -285,143 +295,6 @@
 							</div>
 						</div>
                         @endif
-						<div class="col-md-6 clinic">
-							<div class="box_list wow fadeIn">
-								<a href="#0" class="wish_bt"></a>
-								<figure>
-									<a href="{{ url('detail-page') }}"><img src="{{ asset('HI_03/img/hospital/hospital.jpg') }}" class="img-fluid" alt="">
-										<div class="preview showClinic1"><span>Đọc thêm</span></div>
-									</a>
-								</figure>
-								<div class="wrapper">
-									<h3>Bệnh viện Bạch Mai</h3>
-
-									<p>78 Đường Giải Phóng, Phương Đình, Đống Đa, Hà Nội</p>
-									<span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-									<a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="{{ asset('HI_03/img/badges/badge_1.svg') }}" width="15" height="15" alt=""></a>
-								</div>
-								<ul>
-									<li><a href="#0" onclick="onHtmlClick('Doctors', 0)"><i class="icon_pin_alt"></i>Bản đồ (3km)</a></li>
-									<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"><i class="icon_pin_alt"></i>Directions</a></li>
-									<li class="showClinic1"><a href="{{ url('detail-page') }}">Đọc thêm</a></li>
-								</ul>
-							</div>
-						</div>
-						<!-- /box_list -->
-
-						<div class="col-md-6 clinic">
-							<div class="box_list wow fadeIn">
-								<a href="#0" class="wish_bt"></a>
-								<figure>
-									<a href="{{ url('detail-page') }}"><img src="{{ asset('HI_03/img/hospital/hospital.jpg') }}" class="img-fluid" alt="">
-										<div class="preview showClinic2"><span>Đọc thêm</span></div>
-									</a>
-								</figure>
-								<div class="wrapper">
-									<h3>Bệnh viện Trung Ương Quân Đội 108</h3>
-									<p>Số 1 Trần Hưng Đạo, Bạch Đằng, Hai Bà Trưng, Hà Nội </p>
-									<span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-									<a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="{{ asset('HI_03/img/badges/badge_2.svg') }}" width="15" height="15" alt=""></a>
-								</div>
-								<ul>
-									<li><a href="#0" onclick="onHtmlClick('Doctors', 1)"><i class="icon_pin_alt"></i>Bản đồ (3km)</a></li>
-									<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"><i class="icon_pin_alt"></i>Directions</a></li>
-									<li class="showClinic2"><a href="{{ url('detail-page') }}">Đọc thêm</a></li>
-								</ul>
-							</div>
-						</div>
-						<!-- /box_list -->
-
-						<div class="col-md-6 clinic">
-							<div class="box_list wow fadeIn">
-								<a href="#0" class="wish_bt"></a>
-								<figure>
-									<a href="{{ url('detail-page') }}"><img src="{{ asset('HI_03/img/hospital/hospital.jpg') }}" class="img-fluid" alt="">
-										<div class="preview showClinic3"><span>Đọc thêm</span></div>
-									</a>
-								</figure>
-								<div class="wrapper">
-									<h3>Bệnh viện Việt Pháp Hà Nội</h3>
-									<p>1 Phương Mai, Đống Đa, Hà Nội</p>
-									<span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-									<a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="{{ asset('HI_03/img/badges/badge_3.svg') }}" width="15" height="15" alt=""></a>
-								</div>
-								<ul>
-									<li><a href="#0" onclick="onHtmlClick('Doctors', 2)"><i class="icon_pin_alt"></i>Bản đồ (3km)</a></li>
-									<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"><i class="icon_pin_alt"></i>Directions</a></li>
-									<li class="showClinic3"><a href="{{ url('detail-page') }}">Đọc thêm</a></li>
-								</ul>
-							</div>
-						</div>
-						<!-- /box_list -->
-
-						<div class="col-md-6 clinic">
-							<div class="box_list wow fadeIn">
-								<a href="#0" class="wish_bt"></a>
-								<figure>
-									<a href="{{ url('detail-page') }}"><img src="{{ asset('HI_03/img/hospital/hospital.jpg') }}" class="img-fluid" alt="">
-										<div class="preview showClinic4"><span>Đọc thêm</span></div>
-									</a>
-								</figure>
-								<div class="wrapper">
-									<h3>Bệnh viện Việt Nam Cuba</h3>
-									<p>37 Hai Bà Trưng, Tràng Tiền, Hoàn Kiếm, Hà Nội</p>
-									<span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-									<a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="{{ asset('HI_03/img/badges/badge_3.svg') }}" width="15" height="15" alt=""></a>
-								</div>
-								<ul>
-									<li><a href="#0" onclick="onHtmlClick('Doctors', 3)"><i class="icon_pin_alt"></i>Bản đồ (3km)</a></li>
-									<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"><i class="icon_pin_alt"></i>Directions</a></li>
-									<li class="showClinic4"><a href="{{ url('detail-page') }}">Đọc thêm</a></li>
-								</ul>
-							</div>
-						</div>
-						<!-- /box_list -->
-
-						<div class="col-md-6 clinic">
-							<div class="box_list wow fadeIn">
-								<a href="#0" class="wish_bt"></a>
-								<figure>
-									<a href="{{ url('detail-page') }}"><img src="{{ asset('HI_03/img/hospital/hospital.jpg') }}" class="img-fluid" alt="">
-										<div class="preview showClinic5"><span>Đọc thêm</span></div>
-									</a>
-								</figure>
-								<div class="wrapper">
-									<h3>Bệnh viện Tai Mũi Họng Trung ương</h3>
-									<p>78 Giải Phóng, Phương Đình, Đống Đa, Hà Nội</p>
-									<span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-									<a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="{{ asset('HI_03/img/badges/badge_4.svg') }}" width="15" height="15" alt=""></a>
-								</div>
-								<ul>
-									<li><a href="#0" onclick="onHtmlClick('Doctors', 1)"><i class="icon_pin_alt"></i>Bản đồ (3km)</a></li>
-									<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"><i class="icon_pin_alt"></i>Directions</a></li>
-									<li class="showClinic5"><a href="{{ url('detail-page') }}">Đọc thêm</a></li>
-								</ul>
-							</div>
-						</div>
-						<!-- /box_list -->
-
-						<div class="col-md-6 clinic">
-							<div class="box_list wow fadeIn">
-								<a href="#0" class="wish_bt"></a>
-								<figure>
-									<a href="{{ url('detail-page') }}"><img src="{{ asset('HI_03/img/hospital/hospital.jpg') }}" class="img-fluid" alt="">
-										<div class="preview showClinic6"><span>Đọc thêm</span></div>
-									</a>
-								</figure>
-								<div class="wrapper">
-									<h3>Bệnh viện Y Học Cổ Truyền Trung Ương</h3>
-									<p>29 Nguyễn Bỉnh Khiêm, Nguyễn Du, Hai Bà Trưng, Hà Nội</p>
-									<span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-									<a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="{{ asset('HI_03/img/badges/badge_4.svg') }}" width="15" height="15" alt=""></a>
-								</div>
-								<ul>
-									<li><a href="#0" onclick="onHtmlClick('Doctors', 2)"><i class="icon_pin_alt"></i>Bản đồ (3km)</a></li>
-									<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"><i class="icon_pin_alt"></i>Directions</a></li>
-									<li class="showClinic6"><a href="{{ url('detail-page') }}">Đọc thêm</a></li>
-								</ul>
-							</div>
-						</div>
 						<!-- /box_list -->
 						<!-- /box_list -->
 					</div>
