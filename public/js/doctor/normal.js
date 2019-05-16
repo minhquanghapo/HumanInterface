a7e0763db0716e1cc0c3df833df7b467b2c90f63
thanh binh <binh97.lt@gmail.com>
2019-05-16 12:09:25 +0700

    something

--- public/js/doctor/normal.js
+++ public/js/doctor/normal.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
     }, function() {
         $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
     });
+
+    $("#update-info").on("click",function () {
+        $("#infoModal").modal("hide");
+        toastr["success"]("Cập nhật thành công!");
+    });
+
+    $("#update-password").on("click",function () {
+        $("#infoModal").modal("hide");
+        $("#passModal").modal("show");
+    });
+
+    $("#accept-pass").on("click",function () {
+        $("#passModal").modal("hide");
+        toastr["success"]("Cập nhật mật khẩu thành công!");
+    });
 });
